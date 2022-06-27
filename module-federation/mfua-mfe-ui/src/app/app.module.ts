1b93a2ec6ff17b8f199751e6343bed5aeee7692e
Filippo Malvisi <fmalvisi@imolainformatica.it>
2022-06-27 11:13:13 +0200

    basic standalone code for ui component

--- module-federation/mfua-mfe-ui/src/app/app.module.ts
+++ module-federation/mfua-mfe-ui/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { UlComponent } from './components/ul/ul.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    UlComponent
   ],
   imports: [
     BrowserModule
